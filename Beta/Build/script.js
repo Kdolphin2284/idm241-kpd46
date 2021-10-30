692a51fef1207d692ae74006e517252d32e66e5c
Kdolphin2284 <71986227+Kdolphin2284@users.noreply.github.com>
2021-10-30 16:34:31 -0400

    Functionality Update
    It works.

--- Beta/Build/script.js
+++ Beta/Build/script.js
@@ -1,7 +1,28 @@
 const buttonFlex = document.getElementById('galleryButton');
-const gallery1 = document.getElementById('gallery1')
-const gallery2 = document.getElementById('gallery2')
-const gallery3 = document.getElementById('gallery3')
+let gallery1 = document.getElementById('gallery1')
+let gallery2 = document.getElementById('gallery2')
+let gallery3 = document.getElementById('gallery3')
 
-// buttonFlex.addEventListener("click", )
+
+/////////////////////////////////////////////////
+////////// EVENT LISTENER IF STATEMENT //////////
+/////////////////////////////////////////////////
+
+buttonFlex.addEventListener('click', function(){
+    if (gallery1.classList.contains('galleryImages')) {
+        console.log('Close image gellery')
+        gallery1.classList.remove("galleryImages");
+        gallery2.classList.remove("galleryImages");
+        gallery3.classList.remove("galleryImages");
+        buttonFlex.style.background = '#6372FF';
+        buttonFlex.innerHTML = "Gallery";
+    } else {
+        console.log('Open up image gellery')
+        gallery1.classList.add("galleryImages");
+        gallery2.classList.add("galleryImages");
+        gallery3.classList.add("galleryImages");
+        buttonFlex.style.background = '#FF6363';
+        buttonFlex.innerHTML = "Close";
+    }
+});
 
