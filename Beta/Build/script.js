2a77aed356ea671388f00b1171e14089abd67b59
Kdolphin2284 <71986227+Kdolphin2284@users.noreply.github.com>
2021-10-30 15:53:05 -0400

    Initial Commit
    File creation/editing.

---
+++ Beta/Build/script.js
@@ -0,0 +1,7 @@
+const buttonFlex = document.getElementById('galleryButton');
+const gallery1 = document.getElementById('gallery1')
+const gallery2 = document.getElementById('gallery2')
+const gallery3 = document.getElementById('gallery3')
+
+// buttonFlex.addEventListener("click", )
+
